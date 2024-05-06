34ca43cec6fb01bcd700266c6c00c6bb3d885afc
shanekeenan <123633989+shanekeenan@users.noreply.github.com>
2024-05-06 15:45:55 +0100

    Update assignment04-github.py

--- assignment04-github.py
+++ assignment04-github.py
@@ -1,8 +1,8 @@
 # Assignment 04 from Topic 05 Authentication
 
 # Author: Shane Keenan 
-# date: 23/02/2024 
-# status: Ongoing 
+# date: 01/05/2024 
+# status: complete 
 
 '''
 Assignment description: 
@@ -14,50 +14,50 @@ Handup: Push the program as assignment04-github.py to assignments repository.
 Marks: Marks will be given for the functionality and layout of the code; I do expect minimal comments to indicate you know what the program is doing.
 '''
 
-# ok this is not exactly like I asked you to to in the labs
 import requests
 import json
 from config import apikeys as cfg
+from github import Github
 
-filename = "repos-private.json"
-
-#url = 'https://api.github.com/repos/andrewbeattycourseware/datarepresentation/contents/code'
-url = 'https://api.github.com/repos/andrewbeattycourseware/aprivateone'
-
-# the more basic way of setting authorization
-#headers = {'Authorization': 'token ' + apikey}
-#response = requests.get(url, headers= headers)
-
+# fine-grained token stored in config.py file.  
 apikey = cfg["githubkey"]
-response = requests.get(url, auth = ('token', apikey))
 
-print (response.status_code)
-#print (response.json())
-
-with  open(filename, 'w') as fp:
-    repoJSON = response.json()
-    json.dump(repoJSON, fp, indent=4)
+g = Github(apikey)
+repo = g.get_repo("shanekeenan/aprivateone")
+#print(repo.clone_url)
 
 
+#list the repos in my Github account
+
+#for repo in g.get_user().get_repos():
+ #   print(repo.name)
+    #repo.edit(has_wiki=False)
+    # to see all the available attributes and methods
+    #print(dir(repo))
+
+# find the url for the file "test.txt "
+fileInfo = repo.get_contents("test.txt")
+urlOfFile = fileInfo.download_url
+#print (urlOfFile)
+
+# access the contents of the text file
+response = requests.get(urlOfFile)
+contentOfFile = response.text
+#print (contentOfFile)
+
+# count the number of times Andrew appears in the text 
+count = 0
+for char in contentOfFile.split(): # split the text into individual words
+    if char == 'Andrew':
+        count += 1
+print(f"File contains {count} instances of the name Andrew")
 
 
+# Replace Andrew with Shane 
+mod_contentOfFile = contentOfFile.replace('Andrew', 'Shane')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# commit and push to repo 
+gitHubResponse=repo.update_file(fileInfo.path,"Andrew has been updated to Shane",mod_contentOfFile,fileInfo.sha)
+print (gitHubResponse)
+print (f"These have now been replace with the name Shane")
